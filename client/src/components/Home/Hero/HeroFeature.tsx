3c370d1558ba96c171ee278c1ab26f5afb1a9050
Picces04 <khanhhaivodoi@gmail.com>
2025-09-27 22:49:36 +0700

    Update Header & Footer & Category

--- client/src/components/Home/Hero/HeroFeature.tsx
+++ client/src/components/Home/Hero/HeroFeature.tsx
@@ -1,46 +1,48 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 const featureData = [
-  {
-    img: "/images/icons/icon-01.svg",
-    title: "Free Shipping",
-    description: "For all orders $200",
-  },
-  {
-    img: "/images/icons/icon-02.svg",
-    title: "1 & 1 Returns",
-    description: "Cancellation after 1 day",
-  },
-  {
-    img: "/images/icons/icon-03.svg",
-    title: "100% Secure Payments",
-    description: "Gurantee secure payments",
-  },
-  {
-    img: "/images/icons/icon-04.svg",
-    title: "24/7 Dedicated Support",
-    description: "Anywhere & anytime",
-  },
+    {
+        img: '/images/icons/icon-01.svg',
+        title: 'Miễn Phí Vận Chuyển',
+        description: 'Cho mọi đơn hàng trên 500k',
+    },
+    {
+        img: '/images/icons/icon-03.svg',
+        title: '100% Thanh Toán An Toàn',
+        description: 'Đảm bảo an toàn tuyệt đối',
+    },
+    {
+        img: '/images/icons/icon-04.svg',
+        title: '24/7 Hỗ Trợ',
+        description: 'Bất cứ lúc nào bạn cần',
+    },
 ];
 
 const HeroFeature = () => {
-  return (
-    <div className="max-w-[1060px] w-full mx-auto px-4 sm:px-8 xl:px-0">
-      <div className="flex flex-wrap items-center gap-7.5 xl:gap-12.5 mt-10">
-        {featureData.map((item, key) => (
-          <div className="flex items-center gap-4" key={key}>
-            <Image src={item.img} alt="icons" width={40} height={41} />
+    return (
+        <div className="max-w-[1060px] w-full mx-auto px-4 sm:px-8 xl:px-0">
+            <div className="flex flex-wrap items-center gap-7.5 xl:gap-12.5 mt-10">
+                {featureData.map((item, key) => (
+                    <div className="flex items-center gap-4" key={key}>
+                        <Image
+                            src={item.img}
+                            alt="icons"
+                            width={40}
+                            height={41}
+                        />
 
-            <div>
-              <h3 className="font-medium text-lg text-dark">{item.title}</h3>
-              <p className="text-sm">{item.description}</p>
+                        <div>
+                            <h3 className="font-medium text-lg text-dark">
+                                {item.title}
+                            </h3>
+                            <p className="text-sm">{item.description}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default HeroFeature;
